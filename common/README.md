83c01173c5ab6e11ea48129004129afe8ee6f664
wangshengqiang <527538586@qq.com>
2025-03-05 16:47:07 +0800

    [update]新增图片压缩方法

--- common/README.md
+++ common/README.md
@@ -10,9 +10,13 @@
 
 ## Step 2. Add the dependency
 
-* api 'com.github.shengwang520:common-ktx:1.0.05'
+* api 'com.github.shengwang520:common-ktx:1.0.06'
 
 ## Changelog
+
+### Version 1.0.06
+
+* 新增图片压缩方法
 
 ### Version 1.0.05
 
--- common/src/main/java/com/sheng/wang/common/helper/FileHelper.kt
+++ common/src/main/java/com/sheng/wang/common/helper/FileHelper.kt
@@ -1,10 +1,17 @@
 package com.sheng.wang.common.helper
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Environment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
+import kotlin.math.min
 
 /**
  * create file
@@ -36,26 +43,89 @@ fun String?.deleteFile() {
 /**
  * uri to file path
  */
-fun Context.uriToPath(uri: Uri): String? {
+fun Context.uriToPath(uri: Uri, isCompress: Boolean = true, minSize: Int = 1080): String? {
     try {
-        val imgFile = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        if (imgFile?.exists() == false) {
-            imgFile.mkdir()
-        }
-        val file = File(imgFile?.absolutePath + File.separator + System.currentTimeMillis() + ".jpg")
         val fileInputStream = contentResolver?.openInputStream(uri)
-        val fileOutputStream = FileOutputStream(file)
-        val buffer = ByteArray(1024)
-        var byteRead: Int
-        while (-1 != fileInputStream?.read(buffer).also { byteRead = it ?: 0 }) {
-            fileOutputStream.write(buffer, 0, byteRead)
+        val bitmap = BitmapFactory.decodeStream(fileInputStream)
+        return if (isCompress) {
+            bitmap.compress(minSize).savePath(this)
+        } else {
+            bitmap.savePath(this)
         }
-        fileInputStream?.close()
-        fileOutputStream.flush()
-        fileOutputStream.close()
-        return file.absolutePath
     } catch (e: Exception) {
         e.printStackTrace()
+    }
+    return null
+}
+
+/**
+ * path to bitmap
+ */
+fun String?.toBitmap(): Bitmap? {
+    this?.let {
+        var bitmap: Bitmap
+        try {
+            bitmap = BitmapFactory.decodeFile(it)
+        } catch (e: OutOfMemoryError) {
+            e.printStackTrace()
+            System.gc()
+            val opts = BitmapFactory.Options()
+            opts.inSampleSize = 4
+            bitmap = BitmapFactory.decodeFile(it, opts)
+        }
+        return bitmap
+    }
+    return null
+}
+
+/**
+ * bitmap compress scale
+ * @param minSize compress mis size
+ */
+fun Bitmap.compress(minSize: Int = 1080): Bitmap {
+    val width = this.width
+    val height = this.height
+    if (min(width, height) > minSize) {
+        val newWidth: Int
+        val newHeight: Int
+        if (width > height) {
+            newHeight = minSize
+            newWidth = (width / height.toFloat() * newHeight).toInt()
+        } else if (width < height) {
+            newWidth = minSize
+            newHeight = (height / width.toFloat() * newWidth).toInt()
+        } else {
+            newHeight = minSize
+            newWidth = newHeight
+        }
+        val scaleWidth = newWidth.toFloat() / width
+        val scaleHeight = newHeight.toFloat() / height
+        val matrix = Matrix()
+        matrix.postScale(scaleWidth, scaleHeight)
+        val results = Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+        this.recycle()
+        return results
+    }
+    return this
+}
+
+/**
+ * bitmap save jpg file
+ */
+fun Bitmap.savePath(context: Context?): String? {
+    context?.let {
+        val file = it.createFile(Environment.DIRECTORY_PICTURES, ".jpg")
+        var fos: FileOutputStream? = null
+        try {
+            fos = FileOutputStream(file)
+            this.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.flush()
+            return file?.absolutePath
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            fos?.close()
+        }
     }
     return null
 }
@@ -72,4 +142,15 @@ fun String?.getFileName(): String {
         }
     }
     return System.currentTimeMillis().toString()
+}
+
+/**
+ * compress image
+ * @param path local file path
+ * @param minSize compress minSize default 1080P
+ */
+suspend fun Context?.compress(path: String, minSize: Int = 1080): String {
+    return withContext(Dispatchers.IO) {
+        path.toBitmap()?.compress(minSize)?.savePath(this@compress) ?: path
+    }
 }
